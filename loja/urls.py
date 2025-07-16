11e97a52d1bf088400219b8211f77df3545cf06e
AzevedoDev <154917474+LucasCsAzevedo@users.noreply.github.com>
2025-07-16 20:38:33 -0300

    first commit - aula 4 - ecommerce/urls e loja/urls

---
+++ loja/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+
+from .views import * # ou from . import views ai para cada view preciso colocar o prefixo views.
+
+app_name = "loja"
+
+urlpatterns = [
+    path('/', homepage, name="homepage") # Como configurar: ('caminho' ou link | a view que será carregada naquela página | nome interno: referenciar dentro do nosso site)
+]
