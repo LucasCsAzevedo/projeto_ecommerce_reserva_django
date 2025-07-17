719bccfa8cc21973042a1e0279340f0cfb06aaa1
AzevedoDev <154917474+LucasCsAzevedo@users.noreply.github.com>
2025-07-17 20:04:28 -0300

    aula 08: static files and example css

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -10,7 +10,9 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
+import os
 from pathlib import Path
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -116,6 +118,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATICFILES_DIRS = [ # Direcionando o caminho dos nossos arquivos estáticos, Aula 8
+    os.path.join(BASE_DIR, 'static')    
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
