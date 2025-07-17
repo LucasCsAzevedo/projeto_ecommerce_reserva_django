c0a554c16bd0c3afb3fcc091f9d704b9234ae94b
AzevedoDev <154917474+LucasCsAzevedo@users.noreply.github.com>
2025-07-17 20:30:11 -0300

    aula 10: navbar and {% include %}

--- loja/urls.py
+++ loja/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 
 from .views import * # ou from . import views ai para cada view preciso colocar o prefixo views.
 
-app_name = "loja"
+# app_name = "loja"
 
 urlpatterns = [
     path('', homepage, name="homepage"), # Como configurar: ('caminho' ou link | a view que será carregada naquela página | nome interno: referenciar dentro do nosso site)
