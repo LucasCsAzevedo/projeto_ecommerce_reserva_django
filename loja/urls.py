e8f92e6fc1e786aed881734e5da0a10ecfe80d1f
AzevedoDev <154917474+LucasCsAzevedo@users.noreply.github.com>
2025-07-16 21:10:08 -0300

    aula 06: create links and templates

--- loja/urls.py
+++ loja/urls.py
@@ -5,5 +5,10 @@ from .views import * # ou from . import views ai para cada view preciso colocar 
 app_name = "loja"
 
 urlpatterns = [
-    path('', homepage, name="homepage") # Como configurar: ('caminho' ou link | a view que será carregada naquela página | nome interno: referenciar dentro do nosso site)
+    path('', homepage, name="homepage"), # Como configurar: ('caminho' ou link | a view que será carregada naquela página | nome interno: referenciar dentro do nosso site)
+    path('loja/', loja, name="loja"), # Aula 6
+    path('minhaconta/', minha_conta, name="minha_conta"), # Aula 6
+    path('login/', login, name="login"), # Aula 6
+    path('carrinho/', carrinho, name="carrinho"), # Aula 6
+    path('checkout/', checkout, name="checkout"), # Aula 6
 ]
--- loja/views.py
+++ loja/views.py
@@ -9,3 +9,23 @@ from django.shortcuts import render
 # Create your views here.
 def homepage(request): # Toda função dentro da minha views precisa receber uma requisição, seja ela do tipo GET ou POST, Aula 5
     return render(request, 'homepage.html') # Por padrão a view precisa renderizar algo (render) através da requisição, uma homepage, Aula 5
+
+
+def loja(request):
+    return render(request, 'loja.html')
+
+
+def carrinho(request):
+    return render(request, 'carrinho.html')
+
+
+def checkout(request):
+    return render(request, 'checkout.html')
+
+
+def minha_conta(request): # template de autenticação do usuário
+    return render(request, 'user/minha_conta.html')
+
+
+def login(request): # template de autenticação do usuário
+    return render(request, 'user/login.html')
