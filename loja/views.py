c0f3e1785982963c4e06c8b25753cf8ec72b83e2
AzevedoDev <154917474+LucasCsAzevedo@users.noreply.github.com>
2025-07-29 21:48:39 -0300

    aula 20: add banners and filter

--- loja/views.py
+++ loja/views.py
@@ -9,7 +9,10 @@ from .models import * # importando os nossos modelos para usar nas nossas views
 
 # Create your views here.
 def homepage(request): # Toda função dentro da minha views precisa receber uma requisição, seja ela do tipo GET ou POST, Aula 5
-    return render(request, 'homepage.html') # Por padrão a view precisa renderizar algo (render) através da requisição, uma homepage, Aula 5
+    banners = Banner.objects.all()
+    context = {"banners": banners}
+    
+    return render(request, 'homepage.html', context) # Por padrão a view precisa renderizar algo (render) através da requisição, uma homepage, Aula 5
 
 
 def loja(request):
