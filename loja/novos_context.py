bac52e6828e9763d627458f2b8fc06cfa84aa4ce
AzevedoDev <154917474+LucasCsAzevedo@users.noreply.github.com>
2025-08-04 21:29:22 -0300

    aula 32: add itens no carrinho

--- loja/novos_context.py
+++ loja/novos_context.py
@@ -7,7 +7,7 @@ def carrinho(request):
     if request.user.is_authenticated:
         cliente = request.user.cliente # Consigo pegar o cliente através do user, como existe um relacionamento OnetoOne eu poderia fazer isso inverso: cliente.user
     else:
-        print('não logado')
+        return {"quantidade_produtos_carrinho": quantidade_produtos_carrinho}
         
     pedido, criado = Pedido.objects.get_or_create(cliente=cliente, finalizado=False) # O django já tem essa função para caso não encontrar o pedido, criar ele.
     itens_pedido = ItensPedido.objects.filter(pedido=pedido)
