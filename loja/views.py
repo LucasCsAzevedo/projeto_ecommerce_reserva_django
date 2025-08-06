b31d0c9bad545c1bf26792bc4d9c42da66d2db6c
AzevedoDev <154917474+LucasCsAzevedo@users.noreply.github.com>
2025-08-05 21:00:42 -0300

    aula 34: aumentar itens do carrinho

--- loja/views.py
+++ loja/views.py
@@ -52,14 +52,29 @@ def ver_produto(request, id_produto, id_cor=None):
 def adicionar_carrinho(request, id_produto):
     if request.method == 'POST' and id_produto:
         dados = request.POST.dict()
-        print(dados)
         tamanho = dados.get("tamanho")
         id_cor = dados.get("cor")
         if not tamanho:
             return redirect('loja')
+        
+        if request.user.is_authenticated:
+            cliente = request.user.cliente
+        else:
+            redirect('loja')
+            
+        pedido, criado = Pedido.objects.get_or_create(cliente=cliente, finalizado=False)
+        item_estoque = ItemEstoque.objects.get(produto__id=id_produto, tamanho=tamanho, cor__id=id_cor)
+        item_pedido, criado = ItensPedido.objects.get_or_create(item_estoque=item_estoque, pedido=pedido)
+        item_pedido.quantidade += 1
+        item_pedido.save()
+        
         return redirect('carrinho')
     else:
         return redirect('loja') # Redirecionando para nossa loja, através do nome que definimos na url
+    
+    
+def remover_carrinho(request):
+    return redirect('carrinho')
 
 
 def carrinho(request):
